6d7a813bc9e8fbeb712a34e9651412d14d91dbe5
JF <54219775+jfmongrain@users.noreply.github.com>
2025-03-22 18:12:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,16 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
 # Enable COPR repos
 
 dnf5 -y copr enable ilyaz/LACT
@@ -26,102 +16,102 @@ dnf5 -y copr enable pesader/hblock
       
 # Install packages 
 
-dnf5 -y install\
- langpacks-core-fr\
- langpacks-fr\
- lact-libadwaita\
- hblock
+dnf5 -y install \
+langpacks-core-fr \
+langpacks-fr \
+lact-libadwaita \
+hblock
 
 # Remove packages
 
-dnf5 -y remove\
- ibus-hangul\
- ibus-libpinyin\
- ibus-anthy\
- ibus-anthy-python\
- ibus-m17n\
- gnome-tour\
- yelp\
- solaar\
- input-leap\
- simple-scan\
- gnome-system-monitor\
- gnome-classic-session\
- gnome-shell-extension-apps-menu\
- gnome-shell-extension-logo-menu\
- gnome-shell-extension-launch-new-instance\
- gnome-shell-extension-places-menu\
- gnome-shell-extension-appindicator\
- gnome-shell-extension-dash-to-dock\
- gnome-shell-extension-caffeine\
- gnome-shell-extension-window-list\
- gnome-shell-extension-blur-my-shell\
- gnome-shell-extension-search-light\
- gnome-shell-extension-user-theme\
- gnome-shell-extension-tailscale-gnome-qs\
- nautilus-gsconnect\
- gnome-shell-extension-gsconnect\
- gnome-shell-theme-yaru\
- yaru-sound-theme\
- yaru-icon-theme\
- yaru-gtksourceview-theme\
- yaru-gtk4-theme\
- yaru-gtk3-theme\
- yaru-gtk2-theme\
- yaru-theme\
- input-remapper
+dnf5 -y remove \
+ibus-hangul \
+ibus-libpinyin \
+ibus-anthy \
+ibus-anthy-python \
+ibus-m17n \
+gnome-tour \
+yelp \
+solaar \
+input-leap \
+simple-scan \
+gnome-system-monitor \
+gnome-classic-session \
+gnome-shell-extension-apps-menu \
+gnome-shell-extension-logo-menu \
+gnome-shell-extension-launch-new-instance \
+gnome-shell-extension-places-menu \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-dash-to-dock \
+gnome-shell-extension-caffeine \
+gnome-shell-extension-window-list \
+gnome-shell-extension-blur-my-shell \
+gnome-shell-extension-search-light \
+gnome-shell-extension-user-theme \
+gnome-shell-extension-tailscale-gnome-qs \
+nautilus-gsconnect \
+gnome-shell-extension-gsconnect \
+gnome-shell-theme-yaru \
+yaru-sound-theme \
+yaru-icon-theme \
+yaru-gtksourceview-theme \
+yaru-gtk4-theme \
+yaru-gtk3-theme \
+yaru-gtk2-theme \
+yaru-theme \
+input-remapper
 
 # Remove unused fonts
 
-dnf5 -y remove\
- google-noto-fonts-all\
- google-noto-sans-mono-cjk-vf-fonts\
- google-noto-serif-cjk-vf-fonts\
- google-noto-naskh-arabic-vf-fonts\
- google-noto-sans-arabic-vf-fonts\
- google-noto-sans-armenian-vf-fonts\
- google-noto-sans-bengali-vf-fonts\
- google-noto-sans-canadian-aboriginal-vf-fonts\
- google-noto-sans-cherokee-vf-fonts\
- google-noto-sans-devanagari-vf-fonts\
- google-noto-sans-ethiopic-vf-fonts\
- google-noto-sans-georgian-vf-fonts\
- google-noto-sans-gujarati-vf-fonts\
- google-noto-sans-gurmukhi-vf-fonts\
- google-noto-sans-hebrew-vf-fonts\
- google-noto-sans-kannada-vf-fonts\
- google-noto-sans-khmer-vf-fonts\
- google-noto-sans-lao-vf-fonts\
- google-noto-sans-meetei-mayek-vf-fonts\
- google-noto-sans-ol-chiki-vf-fonts\
- google-noto-sans-oriya-vf-fonts\
- google-noto-sans-sinhala-vf-fonts\
- google-noto-sans-symbols-2-fonts\
- google-noto-sans-symbols-vf-fonts\
- google-noto-sans-tamil-vf-fonts\
- google-noto-sans-telugu-vf-fonts\
- google-noto-sans-thaana-vf-fonts\
- google-noto-sans-thai-vf-fonts\
- google-noto-serif-armenian-vf-fonts\
- google-noto-serif-bengali-vf-fonts\
- google-noto-serif-devanagari-vf-fonts\
- google-noto-serif-ethiopic-vf-fonts\
- google-noto-serif-georgian-vf-fonts\
- google-noto-serif-gujarati-vf-fonts\
- google-noto-serif-gurmukhi-vf-fonts\
- google-noto-serif-hebrew-vf-fonts\
- google-noto-serif-kannada-vf-fonts\
- google-noto-serif-khmer-vf-fonts\
- google-noto-serif-lao-vf-fonts\
- google-noto-serif-oriya-vf-fonts\
- google-noto-serif-sinhala-vf-fonts\
- google-noto-serif-tamil-vf-fonts\
- google-noto-serif-telugu-vf-fonts\
- google-noto-serif-thai-vf-fonts\
- google-noto-sans-balinese-fonts\
- google-noto-sans-cjk-fonts\
- google-noto-sans-javanese-fonts\
- google-noto-sans-sundanese-fonts
+dnf5 -y remove \
+google-noto-fonts-all \
+google-noto-sans-mono-cjk-vf-fonts \
+google-noto-serif-cjk-vf-fonts \
+google-noto-naskh-arabic-vf-fonts \
+google-noto-sans-arabic-vf-fonts \
+google-noto-sans-armenian-vf-fonts \
+google-noto-sans-bengali-vf-fonts \
+google-noto-sans-canadian-aboriginal-vf-fonts \
+google-noto-sans-cherokee-vf-fonts \
+google-noto-sans-devanagari-vf-fonts \
+google-noto-sans-ethiopic-vf-fonts \
+google-noto-sans-georgian-vf-fonts \
+google-noto-sans-gujarati-vf-fonts \
+google-noto-sans-gurmukhi-vf-fonts \
+google-noto-sans-hebrew-vf-fonts \
+google-noto-sans-kannada-vf-fonts \
+google-noto-sans-khmer-vf-fonts \
+google-noto-sans-lao-vf-fonts \
+google-noto-sans-meetei-mayek-vf-fonts \
+google-noto-sans-ol-chiki-vf-fonts \
+google-noto-sans-oriya-vf-fonts \
+google-noto-sans-sinhala-vf-fonts \
+google-noto-sans-symbols-2-fonts \
+google-noto-sans-symbols-vf-fonts \
+google-noto-sans-tamil-vf-fonts \
+google-noto-sans-telugu-vf-fonts \
+google-noto-sans-thaana-vf-fonts \
+google-noto-sans-thai-vf-fonts \
+google-noto-serif-armenian-vf-fonts \
+google-noto-serif-bengali-vf-fonts \
+google-noto-serif-devanagari-vf-fonts \
+google-noto-serif-ethiopic-vf-fonts \
+google-noto-serif-georgian-vf-fonts \
+google-noto-serif-gujarati-vf-fonts \
+google-noto-serif-gurmukhi-vf-fonts \
+google-noto-serif-hebrew-vf-fonts \
+google-noto-serif-kannada-vf-fonts \
+google-noto-serif-khmer-vf-fonts \
+google-noto-serif-lao-vf-fonts \
+google-noto-serif-oriya-vf-fonts \
+google-noto-serif-sinhala-vf-fonts \
+google-noto-serif-tamil-vf-fonts \
+google-noto-serif-telugu-vf-fonts \
+google-noto-serif-thai-vf-fonts \
+google-noto-sans-balinese-fonts \
+google-noto-sans-cjk-fonts \
+google-noto-sans-javanese-fonts \
+google-noto-sans-sundanese-fonts
 
 dnf5 -y autoremove
 
