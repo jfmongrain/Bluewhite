34e3e439640a435c711fceb7e8dfe51c8cce61eb
JF <54219775+jfmongrain@users.noreply.github.com>
2025-03-22 17:25:09 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -73,53 +73,53 @@ dnf5 -y remove input-remapper
 # Remove unused fonts
 
 dnf5 -y remove google-noto-fonts-all\
-google-noto-sans-mono-cjk-vf-fonts\
-google-noto-serif-cjk-vf-fonts\
-google-noto-naskh-arabic-vf-fonts\
-google-noto-sans-arabic-vf-fonts\
-google-noto-sans-armenian-vf-fonts\
-google-noto-sans-bengali-vf-fonts\
-google-noto-sans-canadian-aboriginal-vf-fonts\
-google-noto-sans-cherokee-vf-fonts\
-google-noto-sans-devanagari-vf-fonts\
-google-noto-sans-ethiopic-vf-fonts\
-google-noto-sans-georgian-vf-fonts\
-google-noto-sans-gujarati-vf-fonts\
-google-noto-sans-gurmukhi-vf-fonts\
-google-noto-sans-hebrew-vf-fonts\
-google-noto-sans-kannada-vf-fonts\
-google-noto-sans-khmer-vf-fonts\
-google-noto-sans-lao-vf-fonts\
-google-noto-sans-meetei-mayek-vf-fonts\
-google-noto-sans-ol-chiki-vf-fonts\
-google-noto-sans-oriya-vf-fonts\
-google-noto-sans-sinhala-vf-fonts\
-google-noto-sans-symbols-2-fonts\
-google-noto-sans-symbols-vf-fonts\
-google-noto-sans-tamil-vf-fonts\
-google-noto-sans-telugu-vf-fonts\
-google-noto-sans-thaana-vf-fonts\
-google-noto-sans-thai-vf-fonts\
-google-noto-serif-armenian-vf-fonts\
-google-noto-serif-bengali-vf-fonts\
-google-noto-serif-devanagari-vf-fonts\
-google-noto-serif-ethiopic-vf-fonts\
-google-noto-serif-georgian-vf-fonts\
-google-noto-serif-gujarati-vf-fonts\
-google-noto-serif-gurmukhi-vf-fonts\
-google-noto-serif-hebrew-vf-fonts\
-google-noto-serif-kannada-vf-fonts\
-google-noto-serif-khmer-vf-fonts\
-google-noto-serif-lao-vf-fonts\
-google-noto-serif-oriya-vf-fonts\
-google-noto-serif-sinhala-vf-fonts\
-google-noto-serif-tamil-vf-fonts\
-google-noto-serif-telugu-vf-fonts\
-google-noto-serif-thai-vf-fonts\
-google-noto-sans-balinese-fonts\
-google-noto-sans-cjk-fonts\
-google-noto-sans-javanese-fonts\
-google-noto-sans-sundanese-fonts
+ google-noto-sans-mono-cjk-vf-fonts\
+ google-noto-serif-cjk-vf-fonts\
+ google-noto-naskh-arabic-vf-fonts\
+ google-noto-sans-arabic-vf-fonts\
+ google-noto-sans-armenian-vf-fonts\
+ google-noto-sans-bengali-vf-fonts\
+ google-noto-sans-canadian-aboriginal-vf-fonts\
+ google-noto-sans-cherokee-vf-fonts\
+ google-noto-sans-devanagari-vf-fonts\
+ google-noto-sans-ethiopic-vf-fonts\
+ google-noto-sans-georgian-vf-fonts\
+ google-noto-sans-gujarati-vf-fonts\
+ google-noto-sans-gurmukhi-vf-fonts\
+ google-noto-sans-hebrew-vf-fonts\
+ google-noto-sans-kannada-vf-fonts\
+ google-noto-sans-khmer-vf-fonts\
+ google-noto-sans-lao-vf-fonts\
+ google-noto-sans-meetei-mayek-vf-fonts\
+ google-noto-sans-ol-chiki-vf-fonts\
+ google-noto-sans-oriya-vf-fonts\
+ google-noto-sans-sinhala-vf-fonts\
+ google-noto-sans-symbols-2-fonts\
+ google-noto-sans-symbols-vf-fonts\
+ google-noto-sans-tamil-vf-fonts\
+ google-noto-sans-telugu-vf-fonts\
+ google-noto-sans-thaana-vf-fonts\
+ google-noto-sans-thai-vf-fonts\
+ google-noto-serif-armenian-vf-fonts\
+ google-noto-serif-bengali-vf-fonts\
+ google-noto-serif-devanagari-vf-fonts\
+ google-noto-serif-ethiopic-vf-fonts\
+ google-noto-serif-georgian-vf-fonts\
+ google-noto-serif-gujarati-vf-fonts\
+ google-noto-serif-gurmukhi-vf-fonts\
+ google-noto-serif-hebrew-vf-fonts\
+ google-noto-serif-kannada-vf-fonts\
+ google-noto-serif-khmer-vf-fonts\
+ google-noto-serif-lao-vf-fonts\
+ google-noto-serif-oriya-vf-fonts\
+ google-noto-serif-sinhala-vf-fonts\
+ google-noto-serif-tamil-vf-fonts\
+ google-noto-serif-telugu-vf-fonts\
+ google-noto-serif-thai-vf-fonts\
+ google-noto-sans-balinese-fonts\
+ google-noto-sans-cjk-fonts\
+ google-noto-sans-javanese-fonts\
+ google-noto-sans-sundanese-fonts
 
 dnf5 -y autoremove
 
