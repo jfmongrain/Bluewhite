228c846426ddca8e25e191931fdcdeb587a9b6aa
JF <54219775+jfmongrain@users.noreply.github.com>
2025-03-31 17:06:38 -0400

    Update install_hp_plugin.sh

--- install_hp_plugin.sh
+++ install_hp_plugin.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
+# Create user
+useradd -m -p $(echo "dummy" | openssl passwd -1 -stdin) dummy > /dev/null 2>&1
+
 # Install expect
-dnf install -y expect
+dnf5 install -y expect
 
 # Create an expect script
 cat << 'EOF' > install_hp_plugin.exp
 #!/usr/bin/expect
-
-# Set the username and password
-set username "dummy"
-set password "dummy"  ;# Replace with the actual password
-
-# Check if the user exists
-set user_exists [catch {exec id $username} result]
-if {$user_exists} {
-    # User does not exist, create it 
-    exec useradd -p [exec openssl passwd -1 $password] $username
-    puts "User $username created."
-} else {
-    puts "User $username already exists."
-}
 
 # Download the plugin .run file
 exec wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.24.4-plugin.run
 exec wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.24.4-plugin.run.asc
 
 # Run the hp-plugin command
-spawn runuser -u $username -- sh -c "echo 'y' | hp-plugin -p hplip-3.24.4-plugin.run"
+spawn runuser -u dummy -- sh -c "echo 'y' | hp-plugin -p hplip-3.24.4-plugin.run"
 expect "Password:"
-send "$password\r"
+send "dummy\r"
 expect eof
 
 # Clean up: remove downloaded files
 exec rm -f hplip-3.24.4-plugin.run hplip-3.24.4-plugin.run.asc
-
-# Uninstall expect
-exec dnf remove -y expect
-
-# Remove the dummy user
-exec userdel -r $username
-puts "User $username removed."
 EOF
 
 # Make the expect script executable
@@ -50,3 +32,10 @@ chmod +x install_hp_plugin.exp
 
 # Clean up: remove the expect script after execution
 rm -f install_hp_plugin.exp
+
+# Uninstall expect
+dnf5 remove -y expect
+
+# Remove the dummy user
+userdel -r dummy
+echo "User dummy removed."
