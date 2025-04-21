07f39f2dc51da98d47e3c4beafe9d1cbf238d2be
JF <54219775+jfmongrain@users.noreply.github.com>
2025-04-21 10:22:45 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -116,7 +116,7 @@ rm msttcore-fonts-installer-2.6-1.noarch.rpm
 #chmod +x installhpplugin.sh
 #sh installhpplugin.sh
 #rm installhpplugin.sh
-wget -q https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.24.4-plugin.run
+wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.24.4-plugin.run
 chmod +x hplip-3.24.4-plugin.run
 su root -c "yes y | hplip-3.24.4-plugin.run --noprogress --accept --nox11 -- -i"
 
