4438a150a4d1c50b3b877a147be283d1bb391ef6
JF <54219775+jfmongrain@users.noreply.github.com>
2025-04-24 11:30:39 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -103,15 +103,21 @@ langpacks-core-fr \
 langpacks-fr \
 hblock \
 gnome-system-monitor \
-cabextract \
 simple-scan \
-papirus-icon-theme \
-xorg-x11-font-utils
+papirus-icon-theme
+#xorg-x11-font-utils
+#cabextract \
 
 # Install Microsoft fonts
 
-rpm -i msttcore-fonts-installer-2.6-1.noarch.rpm
-rm msttcore-fonts-installer-2.6-1.noarch.rpm
+#rpm -i msttcore-fonts-installer-2.6-1.noarch.rpm
+#rm msttcore-fonts-installer-2.6-1.noarch.rpm
+
+rpm -i webcore-fonts-3.0-1.noarch.rpm
+rpm -i webcore-fonts-vista-3.0-1.noarch.rpm
+
+rm webcore-fonts-3.0-1.noarch.rpm
+rm webcore-fonts-vista-3.0-1.noarch.rpm
 
 # Disable COPR repos
 
