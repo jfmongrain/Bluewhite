d6e818a5f3e3123054998033b912a46b2c4b9619
JF <54219775+jfmongrain@users.noreply.github.com>
2025-04-26 21:32:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # Enable COPR repos
 
 dnf5 -y copr enable pesader/hblock
-dnf5 -y copr enable gloriouseggroll/nobara-42 
+#dnf5 -y copr enable gloriouseggroll/nobara-42 
 
 # Remove packages
 
@@ -107,8 +107,8 @@ langpacks-core-fr \
 langpacks-fr \
 hblock \
 gnome-system-monitor \
-simple-scan \
-flatpost
+simple-scan
+#flatpost
 
 # Install Microsoft fonts
 
@@ -121,7 +121,7 @@ rm webcore-fonts-vista-3.0-1.noarch.rpm
 # Disable COPR repos
 
 dnf5 -y copr disable pesader/hblock
-dnf5 -y copr disable gloriouseggroll/nobara-42 
+#dnf5 -y copr disable gloriouseggroll/nobara-42 
 
 #### Example for enabling a System Unit File
 
