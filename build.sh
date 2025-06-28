eb549ed1ea71689c3414ad8feba58810505d7e01
JF <54219775+jfmongrain@users.noreply.github.com>
2025-06-28 17:33:52 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -ouex pipefail
 
 # Change kernel version 
 
+dnf -y install centos-release-hyperscale-experimental
+dnf -y install kernel-6.14.6-0.hs1.hsk.el10
+
+
+
 # Enable COPR repos
 
 #dnf -y --enablerepo copr:copr.fedorainfracloud.org:pesader:hblock
