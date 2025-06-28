a4eb0c031c14ec775e2e865941c9417e39253cea
JF <54219775+jfmongrain@users.noreply.github.com>
2025-06-28 12:47:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -119,7 +119,7 @@ rm webcore-fonts-vista-3.0-1.noarch.rpm
 
 # Change kernel version 
 
-dnf -y swap --allowerasing kernel-6.14.8-0.hs1.hsk.el10 kernel-6.14.6-0.hs1.hsk.el10
+dnf -y swap --allowerasing kernel-6.14.8-0.hs1.hsk.el10 kernel-6.15.4-1.el10
 
 
 # Disable COPR repos
