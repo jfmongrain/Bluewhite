38b1575eb2abd6d6a86ad750415d6380b4ad3cd1
JF <54219775+jfmongrain@users.noreply.github.com>
2025-07-06 16:20:31 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ hplip
 #hblock
 #gnome-system-monitor
 
-wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_42.x86_64.rpm"
+wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && dnf install -y "$PWD/megasync-Fedora_42.x86_64.rpm"
 rm megasync-Fedora_42.x86_64.rpm
 
 
