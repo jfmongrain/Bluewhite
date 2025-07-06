93b5529273fe8a2a1613e42aa62579341497f895
JF <54219775+jfmongrain@users.noreply.github.com>
2025-07-06 16:22:58 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -90,10 +90,6 @@ hplip
 #hblock
 #gnome-system-monitor
 
-wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && dnf install -y "$PWD/megasync-Fedora_42.x86_64.rpm"
-rm megasync-Fedora_42.x86_64.rpm
-
-
 # Disable COPR repos
 
 #dnf -y copr disable pesader/hblock
